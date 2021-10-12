cc37f7647707b0984734d33b4a18d967ed7f71a0
mnguywn <90582041+mnguywn@users.noreply.github.com>
2021-10-11 20:02:50 -0400

    initial upload

---
+++ joe.js
@@ -0,0 +1,20 @@
+var customerName = prompt ("Please enter your name:","");
+var hotdogs = prompt ("How many hotdogs would you like? (Please enter a whole number):", 0);
+var fries = prompt ("How many french fries would you like? (Please enter a whole number):",0);
+var drinks = prompt ("How many drinks would you like? (Please enter a whole number):",0);
+
+var hotdogsCost = 3.75, friesCost = 2.00, drinksCost = 1.50;
+
+var totalHotdogs = (hotdogs * hotdogsCost);
+var totalFries = (fries * friesCost);
+var totalDrinks = (drinks * drinksCost);
+
+var subTotal = (totalHotdogs + totalFries + totalDrinks);
+
+if (subTotal >= 20) {
+    subTotal = ((totalHotdogs + totalFries + totalDrinks) * 0.90);
+}
+
+var tax = (subTotal * 0.0625);
+
+var total = (subTotal + tax)
